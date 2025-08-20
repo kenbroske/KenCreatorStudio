e8ddce22f295dcd7fa72cab88c9a7f9ad84b6154
kenbroske <kenkennynttabrown@gmail.com>
2025-08-19 21:03:34 -0500

    Added interactive buttons

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
+// When "Click Me!" is pressed
+document.getElementById("clickMeBtn").addEventListener("click", function() {
+  alert("You clicked the first button! 🚀");
+});
+
+// When "Alert Me!" is pressed
+document.getElementById("alertBtn").addEventListener("click", function() {
+  alert("Hello from Ken Creator Studio! ⚡");
+});
+
 console.log("Ken Creator Studio is ready to tinker!");
